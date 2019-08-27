17aa3d1c1400794d3204badd3d8c0ac254ac7234
Abhi5252 <abhipatil5252@gmail.com>
2019-08-27 12:38:36 +0530

    new commit 1.0

---
+++ precache-manifest.cb1f2c36da6b8e7f6b423da15bc6d6c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e0bbc401b843cab1314d2f98f35a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0f52e23287a5f66815",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "c15d2c5249eff8af7cb3",
+    "url": "/static/js/2.12dbce6d.chunk.js"
+  },
+  {
+    "revision": "cc0f52e23287a5f66815",
+    "url": "/static/js/main.eda15d13.chunk.js"
+  },
+  {
+    "revision": "779680fd02bca7eed5cd",
+    "url": "/static/js/runtime~main.4e7cd54a.js"
+  }
+]);
